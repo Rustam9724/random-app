f890fefa7debaae8612459f7ab4b411df7811120
Rustam_9724 <Rustam_9724@mail.ru>
2023-02-26 12:48:02 +0500

    init: main page responsive

---
+++ random-app/src/components/Main.jsx
@@ -0,0 +1,35 @@
+function Main() {
+    return <>
+        <header className="header">
+            Выберите рандомайзер
+        </header>
+        <div className="main">
+            <div className="random-card">
+                <p className="random-card__icon">&#128197;</p>
+                <p className="random-card__type">Дата</p>
+            </div>
+            <div className="random-card">
+                <p className="random-card__icon">&#128274;</p>
+                <p className="random-card__type">Пароли</p>
+            </div>
+            <div className="random-card">
+                <p className="random-card__icon"><b>#</b></p>
+                <p className="random-card__type">Число</p>
+            </div>
+            <div className="random-card">
+                <p className="random-card__icon"><b>&#x3B1;</b></p>
+                <p className="random-card__type">Буква</p>
+            </div>
+            <div className="random-card">
+                <p className="random-card__icon">&#x1F44D;&#128078;</p>
+                <p className="random-card__type">Да или нет</p>
+            </div>
+            <div className="random-card">
+                <p className="random-card__icon">&#127912;</p>
+                <p className="random-card__type">Цвета</p>
+            </div>
+        </div>
+    </>
+}
+
+export default Main;
