83eb002dc4e17b99be5064b45507209f2f2b8ff5
Rustam_9724 <Rustam_9724@mail.ru>
2023-02-27 16:31:59 +0500

    init: date randomiser style

--- random-app/src/components/DateRandomiser.jsx
+++ random-app/src/components/DateRandomiser.jsx
@@ -6,47 +6,47 @@ function DateRandomiser() {
 
     function getRandomDay() {
         const days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-        setResult([...result, days[Math.floor(Math.random() * 7)]]);
+        setResult([...result, ` ${days[Math.floor(Math.random() * 7)]}`]);
     }
     
     function getRandomTime() {
         const newTime = `${Math.floor(Math.random() * 24)}:${Math.floor(Math.random() * 60)}`
         if (newTime.match(/^\d{2}:\d{2}$/)) {
-            setResult([...result, newTime]);
+            setResult([...result, ` ${ newTime}`]);
         } else {
             getRandomTime();
         }
     }
 
     function getRandomDate() {
-        setResult([...result, Math.floor(Math.random() * 31) + 1]);     
+        setResult([...result, ` ${Math.floor(Math.random() * 31) + 1}`]);     
     }
 
     function getRandomMonth() {
         const monthes = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-        setResult([...result, monthes[Math.floor(Math.random() * 13)]]);
+        setResult([...result, ` ${monthes[Math.floor(Math.random() * 13)]}`]);
     }
 
     return (
         <>
-        <header className="date-header">
+        <header className="header date-header">
             Случайная дата
         </header>
-        <main className="date-main">
-            <div className="control-panel">
+        <div className="control-panel">
                 <Link to="/">
-                    ← Вернуться
+                    <button className="control-btn">← Вернуться</button>
                 </Link>
-                <button className="btn" onClick={() => setResult([])}>Сбросить</button>
+                <button className="control-btn" onClick={() => setResult([])}>Сбросить</button>
             </div>
+        <main className="date-main">
             <div className="date-result">
                 {result}
             </div>
             <div className="date-buttons">
-                <button className="btn" onClick={getRandomDay}>День недели</button>
-                <button className="btn" onClick={getRandomTime}>Время</button>
-                <button className="btn" onClick={getRandomDate}>Число</button>
-                <button className="btn" onClick={getRandomMonth}>Месяц</button>
+                <button className="date-btn" onClick={getRandomDay}>День недели</button>
+                <button className="date-btn" onClick={getRandomTime}>Время</button>
+                <button className="date-btn" onClick={getRandomDate}>Число</button>
+                <button className="date-btn" onClick={getRandomMonth}>Месяц</button>
             </div>
         </main>
         </>
